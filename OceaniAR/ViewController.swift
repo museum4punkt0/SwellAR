ae0e800e17ba3b1cbc020ab690b0ded4cca5993f
Michael Schröder <mc.schroeder@gmail.com>
2018-09-18 12:05:13 +0200

    Initial Commit

---
+++ OceaniAR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OceaniAR
+//
+//  Created by Michael Schröder on 18.09.18.
+//  Copyright © 2018 Refrakt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
