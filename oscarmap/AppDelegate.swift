a5395e9845f17471cadf8aecdba44ac63b716569
Michael Schröder <mc.schroeder@gmail.com>
2018-09-21 17:36:59 +0200

    Add tool to download & convert OSCAR data
    This is very useful for finding the OSCAR/PODAAC indexes that map to
    the desired lat/lon boundaries.

---
+++ oscarmap/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  oscarmap
+//
+//  Created by Michael Schröder on 21.09.18.
+//  Copyright © 2018 Refrakt. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
+
---
+++ oscarmap/ViewController.swift
@@ -0,0 +1,101 @@
+//
+//  ViewController.swift
+//  oscarmap
+//
+//  Created by Michael Schröder on 21.09.18.
+//  Copyright © 2018 Refrakt. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+    
+    @IBOutlet weak var datasetField: NSTextField!
+    @IBOutlet weak var latMinField: NSTextField!
+    @IBOutlet weak var latMaxField: NSTextField!
+    @IBOutlet weak var lonMinField: NSTextField!
+    @IBOutlet weak var lonMaxField: NSTextField!
+    @IBOutlet weak var downloadButton: NSButton!
+    @IBOutlet weak var progressIndicator: NSProgressIndicator!
+    @IBOutlet weak var progressLabel: NSTextField!
+    
+    var working: Bool = false {
+        didSet {
+            datasetField.isEnabled = !working
+            latMinField.isEnabled = !working
+            latMaxField.isEnabled = !working
+            lonMinField.isEnabled = !working
+            lonMaxField.isEnabled = !working
+            downloadButton.isEnabled = !working
+            if working {
+                progressIndicator.startAnimation(nil)
+            } else {
+                progressIndicator.stopAnimation(nil)
+            }
+            progressLabel.isHidden = !working
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func download(_ sender: Any) {
+        working = true
+        progressLabel.stringValue = "Downloading OSCAR data from PODAAC…"
+        
+        let dataset = datasetField.stringValue
+        let latMin = latMinField.integerValue
+        let latMax = latMaxField.integerValue
+        let lonMin = lonMinField.integerValue
+        let lonMax = lonMaxField.integerValue
+        
+        PODAAC.downloadOSCAR(dataset: dataset, latitude: (latMin,latMax), longitude: (lonMin,lonMax)) { oscar, error in
+            guard let oscar = oscar else {
+                DispatchQueue.main.async {
+                    if let error = error {
+                        NSAlert(error: error).runModal()
+                    }
+                    self.working = false
+                }
+                return
+            }
+            
+            self.progressLabel.stringValue = "Creating texture image from OSCAR data…"
+            guard let (cgImage,metadata) = oscar.createImage() else {
+                DispatchQueue.main.async {
+                    self.working = false
+                }
+                return
+            }
+            
+            
+            let downloadsDirectory = FileManager.default.urls(for: .downloadsDirectory, in: .userDomainMask).first!
+            let datasetUrl = downloadsDirectory.appendingPathComponent(dataset, isDirectory: true)
+            let textureUrl = datasetUrl.appendingPathComponent("texture.png")
+            let metadataUrl = datasetUrl.appendingPathComponent("metadata.json")
+            do {
+                try FileManager.default.createDirectory(at: datasetUrl, withIntermediateDirectories: true, attributes: nil)
+                writeCGImage(cgImage, to: textureUrl)
+                let jsonData = try JSONEncoder().encode(metadata)
+                try jsonData.write(to: metadataUrl)
+            } catch let error {
+                DispatchQueue.main.async {
+                    NSAlert(error: error).runModal()
+                    self.working = false
+                }
+                return
+            }
+            
+            self.working = false
+            NSSound(named: NSSound.Name("Glass"))?.play()
+        }
+    }
+    
+}
+
+@discardableResult func writeCGImage(_ image: CGImage, to destinationURL: URL) -> Bool {
+    guard let destination = CGImageDestinationCreateWithURL(destinationURL as CFURL, kUTTypePNG, 1, nil) else { return false }
+    CGImageDestinationAddImage(destination, image, nil)
+    return CGImageDestinationFinalize(destination)
+}
